62f7debb4ac3fbf3f66e29b7ad02b8f1bd96302e
unknown <arnavaggarwal999000@gmail.com>
2022-03-15 17:09:53 +0530

    frontend for the website

--- api.py
+++ api.py
@@ -1,11 +1,23 @@
+from distutils.log import debug
 from flask import Flask, render_template
 
-app = Flask()
+app = Flask(__name__)
 
 @app.route("/", methods=["get"])
 def home():
-    render_template("index.html")
+    return render_template("index.html")
 
+@app.route("/login", methods=["get"])
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
-    app.run(port=3000)
+    app.run(port=3000, debug=True)
